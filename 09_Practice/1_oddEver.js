825b0e106c63055d985e09b5467fed99bf9551b6
abhishek07patil <abhi78199@gmail.com>
2024-01-26 22:39:21 +0530

    prac1

---
+++ 09_Practice/1_oddEver.js
@@ -0,0 +1,14 @@
+function oddEven(num){
+    var number = ("Enter a number:");
+    number = parseInt(number);
+
+    if(num%2 === 0){
+        console.log(num + " is even");
+    }
+    else{
+        console.log(num + " is odd");
+    }
+}
+oddEven();
+
+
